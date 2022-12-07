8efa6012493ebe94fe242d75d5ecb3bf67d815f1
TonyBSosa <tonyblad09@gmail.com>
2022-12-07 00:48:54 -0600

    tercer commit

--- appestatico.js
+++ appestatico.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const hbs = require('hbs')
 const app = express()
 
 app.use(express.static(__dirname + '/public'))
@@ -11,6 +11,12 @@ app.get('/', (req, res) => {
     res.render('index')
 
 })
+
+app.get('/atendiendo', (req, res) => {
+    res.render('atendiendo')
+
+})
+
 
 app.listen(3000, () => {
     console.log('puerto 3000 arriba');
